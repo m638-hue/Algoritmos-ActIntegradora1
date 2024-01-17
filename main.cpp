6f99451878a98c3281a800d14d227f00de3fc320
mmauro2152 <A01282152@itesm.mx>
2024-01-17 11:15:58 -0600

    Startup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,50 @@
 // =========================================================
-// File:
+// File: main.cpp
 // Author:
+//     Mauro Martinez - A01282152
+//	   Claudia Torres - A01284940		
 // Date: 
 // =========================================================
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void PrefixArray(vector<int>& arr, string pattern)
+{
+	int length = 0;
+	arr[0] = 0;
+
+	for (int i = 1; i < pattern.length(); i++)
+	{
+		if (pattern[i] == pattern[length])
+		{
+			length++;
+			arr[i] = length;
+		}
+		else
+		{
+			if (length != 0)
+			{
+				length = arr[length - 1];
+				i--;
+			}
+			else
+				arr[i] = 0;
+		}
+	}
+}
+
+void KMP(string main, string pattern)
+{
+	vector<int> prefix(0, pattern.length());
+	PrefixArray(prefix, pattern);
+
+
+}
+
+int main()
+{
+
+	return 0;
+}
